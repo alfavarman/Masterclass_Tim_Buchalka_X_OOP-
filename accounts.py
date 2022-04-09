81c3aa52c6db7636a3ece0d7d97bf850c1d8899e
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-04-09 21:02:07 +0700

    protect balance from unwillingly modification

--- accounts.py
+++ accounts.py
@@ -11,32 +11,32 @@ class Account:
         return pytz.utc.localize(utc_time)
 
     def __init__(self, name, balance=0):
-        self.name = name
-        self.balance = balance
-        self.transaction_list = [(Account._current_time(), balance)]
-        print(f"Account created for {self.name} with initial balance of {balance}")
+        self._name = name
+        self.__balance = balance
+        self._transaction_list = [(Account._current_time(), balance)]
+        print(f"Account created for {self._name} with initial balance of {balance}")
 
     def deposit(self, amount):
         if amount >= 0:
-            self.balance += amount
-            print(f'You deposited {amount}. Your Current Balance is: {self.balance}')
-            self.transaction_list.append((Account._current_time(), amount))
+            self.__balance += amount
+            print(f'You deposited {amount}. Your Current Balance is: {self.__balance}')
+            self._transaction_list.append((Account._current_time(), amount))
 
     def withdraw(self, amount):
-        if amount > self.balance:
+        if amount > self.__balance:
             print(f'Withdraw request of: {amount} exceeds available balance ')
             self.show_balance()
             # self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow())))
         elif amount >=0:
-            self.balance -= amount
-            print(f'You withdraw {amount}. Current Balance: {self.balance}')
-            self.transaction_list.append((Account._current_time(), -amount))
+            self.__balance -= amount
+            print(f'You withdraw {amount}. Current Balance: {self.__balance}')
+            self._transaction_list.append((Account._current_time(), -amount))
 
     def show_balance(self):
-        print(f'Your account balance is: {self.balance}')
+        print(f'Your account balance is: {self.__balance}')
 
     def show_transactions(self):
-        for date, amount in self.transaction_list:
+        for date, amount in self._transaction_list:
             if amount > 0:
                 tran_type = 'deposit'
             else:
@@ -61,3 +61,19 @@ if __name__ == '__main__':
     Luq.deposit(350)
     Luq.withdraw(200)
     Luq.show_transactions()
+    Luq.show_balance()
+    print('#' * 60)
+
+    print('Luq.balance = 900000 - can overwrite the balance\n'
+          'to avoid balance need to be refactor as constant by adding _')
+    Luq.balance = 900000
+    Luq.show_balance()
+    print('now however the we can use _balance method to access balance \n'
+          'and modify it Luq._balance = 10000')
+    Luq.__balance = 10000
+    Luq.show_balance()
+    print('double __will "hide" the access - will change its name\n'
+          'and will need print(Luq.__dict__) to find it and modify.')
+    print(Luq.__dict__)
+
+
