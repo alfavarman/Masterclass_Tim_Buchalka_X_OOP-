68933bb3a88c95fed74a74d8e8c765ded23a02f1
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-06-06 13:28:21 +0700

    protect balance from unwillingly modification

---
+++ accounts 2.py
@@ -0,0 +1,51 @@
+import datetime
+import time
+import pytz
+
+class Account:
+
+
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+        self.transaction_list = []
+        print(f'Account Created For {self.name} with Initial Balance: {self.balance}')
+
+    def deposit(self, amount):
+        if 0 < amount:
+            self.balance += amount
+            print(f'{amount} Successfully deposited into {self.name} Account.')
+            self.show_balance()
+            self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            print(f'{amount} Successfully withdrawn from {self.name} Account.')
+            self.show_balance()
+            self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), (-amount)))
+        else:
+            print(f'Invalid operation. Insufficient funds')
+
+    def show_balance(self):
+        print(f'Current Balance is: {self.balance}')
+
+    def show_transactions(self):
+        for date, amount in self.transaction_list:
+            if amount > 0:
+                transaction_type = 'deposited'
+            else:
+                transaction_type = 'withdrawn'
+                amount *= -1
+            print(f'{amount:6} {transaction_type} on {date} local time was {date.astimezone()}')
+
+
+luq = Account('Luq', 200)
+luq.deposit(300)
+luq.show_balance()
+luq.deposit(3000)
+luq.show_balance()
+luq.withdraw(4000)
+luq.withdraw(2340)
+luq.show_balance()
+luq.show_transactions()
